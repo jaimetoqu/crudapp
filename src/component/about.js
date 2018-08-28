a65975e5f8f3a0916e2b9d41681ae95573d7dc20
Jaime Torres <jaime@ormasoft.cl>
2018-08-28 18:29:54 -0300

    http requests with axios

--- src/component/about.js
+++ src/component/about.js
@@ -1,16 +1,54 @@
 import React from "react";
+import axios from "axios";
 
-class AboutScreen extends React.Component {
+export default class AboutScreen extends React.Component {
+
     constructor(props) {
         super();
         this.state = {
-            user: {}
+            persons: []
         };
+        this.baseUrl = "https://jsonplaceholder.typicode.com";
+    }
+
+    componentDidMount() {
+        axios.get(this.baseUrl + '/users').then(response => {
+            console.log(response.data);
+            this.setState({ persons: response.data });
+        })
+        .catch(response => {
+            console.log(response);
+        });
+    }
+
+    submit(e) {
+        e.preventDefault();
+        let person = {
+            name: this.refs.name.value
+        }
+        axios.post(this.baseUrl + '/users', {person}).then(response => {
+            console.log(response.data);
+        })
+        .catch(response => {
+            console.log(response);
+        });
     }
 
     render() {
-        return (<div>Aboutttt</div>);
+        return (
+            <div>Aboutttt
+                <form ref="myForm">
+                    <input type="text" placeholder="Full Name" ref="name"/>
+                    <button onClick={(e) => this.submit(e)} >Submit</button>
+                </form>
+                <ul>
+                    {this.state.persons.map((person, i) =>
+                        <li key={person.id}>
+                            {person.id}: {person.name}
+                        </li>
+                    )}
+                </ul>
+            </div>
+        );
     }
 }
-
-export default AboutScreen;
